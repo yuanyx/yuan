d10b59dd051994aa455e2dfcb9ee3bb9d690646f
yuan <yuanyx_155@163.com>
2015-11-30 09:56:25 +0800

    初始化项目

---
+++ 01ScrollView/01ScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01ScrollView
+//
+//  Created by 袁艳祥 on 15/10/20.
+//  Copyright © 2015年 袁艳祥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 01ScrollView/01ScrollView/ViewController.m
@@ -0,0 +1,49 @@
+//
+//  ViewController.m
+//  01ScrollView
+//
+//  Created by 袁艳祥 on 15/10/20.
+//  Copyright © 2015年 袁艳祥. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) UIImage *image;
+@property (strong, nonatomic) UIImageView *imgView;
+
+@end
+
+@implementation ViewController
+
+- (void)setImage:(UIImage *)image
+{
+    _image = image;
+    self.imgView.image = image;
+    [self.imgView sizeToFit];
+    self.scrollView.contentSize = image.size;
+}
+
+- (UIImageView *)imgView
+{
+    if (_imgView == nil) {
+        _imgView = [[UIImageView alloc] init];
+        [self.scrollView addSubview:_imgView];
+    }
+    return _imgView;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.image = [UIImage imageNamed:@"minion.png"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
