de483bc9334d64a65f5b65b594e4977f83803ca1
yuan <yuanyx_155@163.com>
2015-11-30 10:07:55 +0800

    改变view的背景颜色

--- 01ScrollView/01ScrollView/ViewController.m
+++ 01ScrollView/01ScrollView/ViewController.m
@@ -39,6 +39,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.image = [UIImage imageNamed:@"minion.png"];
+    self.view.backgroundColor = [UIColor blueColor];
 }
 
 - (void)didReceiveMemoryWarning {
